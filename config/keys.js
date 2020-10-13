d16a0b092232b62d346480eea28a258ea07abe6f
Paul <webdevpaulmason@gmail.com>
2020-10-13 12:59:47 -0700

    Remove security issue

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://admin:birth07032020@cluster0.gasfy.mongodb.net/mern-auth?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://<login><password>@cluster0.gasfy.mongodb.net/mern-auth?retryWrites=true&w=majority",
   secretOrKey: "secret"
 }
